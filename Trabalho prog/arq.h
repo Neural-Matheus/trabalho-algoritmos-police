44a40ca0d16629d3068f369f170f74c0f9969ef3
Matheus Oliveira <72510366+zackkz@users.noreply.github.com>
2023-11-01 22:45:34 -0400

    Add files via upload

--- Trabalho prog/arq.h
+++ Trabalho prog/arq.h
@@ -2,7 +2,11 @@
 #include "viaturaFunctions\adicionarViatura.cpp"
 #include "structs\cad.h"
 #include "viaturaFunctions\viaturasEmUso.cpp"
+#include "pessoaFunctions\pessoasAtuais.cpp"
 
 void viaturaLogin(Nodo* &listViaturas, Nodo* &listViaturasFull);
 
 void viaturaEmUso(Nodo* &listViaturas, Nodo* &listViaturasFull);
+
+void lerPessoasDoArquivo(Nodo* &listPessoas);
+
